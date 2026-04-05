098b5f92f7b16720af395b9e2fbb992e59b0285d
Ruis.Software <53178920+RuisSoftware@users.noreply.github.com>
2026-04-05 15:25:58 +0200

    Pushing to testing phase

--- src/CSM.ExtraLandscapingTools/Mod/ModMetadata.cs
+++ src/CSM.ExtraLandscapingTools/Mod/ModMetadata.cs
@@ -2,7 +2,7 @@ namespace CSM.ExtraLandscapingTools.Mod
 {
     internal static class ModMetadata
     {
-        internal const string Version = "v1.0.0.0";
+        internal const string Version = "v1.1.0.0";
         internal const string ModName = "CSM Extra Landscaping Tools";
         internal const string Description = "Extra Landscaping Tools + Surface Painter combined, with CSM multiplayer sync.";
     }
